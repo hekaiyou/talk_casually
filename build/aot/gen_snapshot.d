fb970c09cbce1d2c34f88559be62c52d9072b0b0
hekaiyou <30341875+hekaiyou@users.noreply.github.com>
2017-11-23 10:33:10 +0800

    删除大文件

--- build/aot/gen_snapshot.d
+++
@@ -1,1 +0,0 @@
-snapshot.d: /Users/hekaiyou/flutter/bin/cache/artifacts/engine/ios-release/gen_snapshot
--- build/app/intermediates/flutter/release/gen_snapshot.d
+++
@@ -1,1 +0,0 @@
-snapshot.d: /Users/hekaiyou/flutter/bin/cache/artifacts/engine/android-arm-release/darwin-x64/gen_snapshot
